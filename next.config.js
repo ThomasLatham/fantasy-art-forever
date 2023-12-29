beb79b2cb93cd1d59c504f94c6d0570a95a8d9df
jameson-and-java <jamesonandjava@gmail.com>
2023-12-29 09:36:01 -0500

    fixing re2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+module.exports = (phase, { defaultConfig }) => ({
+  ...defaultConfig,
+  reactStrictMode: true,
+  webpack(config) {
+    // so that re2 gets prebuilt correctly
+    // see https://github.com/uhop/node-re2/issues/63#issuecomment-815866708
+    config.module.rules.push({
+      test: /re2\.node$/i,
+      use: "raw-loader",
+    });
+
+    return config;
+  },
+});
